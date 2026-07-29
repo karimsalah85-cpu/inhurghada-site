bdac1bbb4c4c642d6895779c696dc2a3eeed5fd9
Karim Salah <karimsalah@Karims-MacBook-Air.local>
2026-07-29 18:32:52 +0300

    Show verified Google review destinations

--- lib/contact.ts
+++ lib/contact.ts
@@ -4,6 +4,7 @@ export const displayPhoneNumber = "+20 103 080 9150";
 export const facebookUrl = "https://www.facebook.com/profile.php?id=61592247695069";
 export const instagramUrl = "https://www.instagram.com/dailyredsea.com7/";
 export const googleReviewUrl = "https://g.page/r/CZO2rT5pTQOXEAI/review";
+export const googleMapsUrl = "https://www.google.com/maps/search/?api=1&query=Daily+Red+Sea&query_place_id=ChIJXZ9eh4x-YCURk7atPmlNA5c";
 
 export function whatsappUrl(message?: string) {
   const baseUrl = `https://wa.me/${whatsappNumber}`;
